6ac692f11b0377cff3d62006f7698d57b3689fb0
Marques Avery <averymarques4@gmail.com>
2020-07-20 16:40:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(number)
+  if number / 3
+    puts "Fizz"
+  elsif number / 5
+    puts "Buzz"
+  else number / 15
+    puts "FizzBuzz"  
 end
