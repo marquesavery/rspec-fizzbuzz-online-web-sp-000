bc01f1482b21925ba1cb04f86b1e3bb84009d8be
Marques Avery <averymarques4@gmail.com>
2020-07-20 17:06:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if number % 3 == true
+  if number % 3 == 0
     puts "Fizz"
-  elsif number % 5 == true
+  elsif number % 5 == 0
     puts "Buzz"
-  else number % 15 == true
+  else number % 15 == 0
     puts "FizzBuzz"
   end
 end
