6a31ee31d34533bbcff90fc41b5c95958f23372d
Marques Avery <averymarques4@gmail.com>
2020-07-20 17:11:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,10 +6,6 @@ def fizzbuzz(number)
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
-  elsif (number % 3) && (number % 5 == 0)
-    "FizzBuzz"
-  elsif (number % 3) || (number % 5 == 0)
-    "FizzBuzz"
   else
   end
 end
